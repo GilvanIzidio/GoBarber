65c9858c2c8611b7f904c7f3329c20c9a50242eb
Gilvan K <gilvam.izidio.dev@gmail.com>
2021-05-14 11:10:10 -0300

    Configurado padrao de codigo com editorconfig e eslint

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
-import express, { request } from 'express';
+import express from 'express';
 
 const app = express();
 
-app.get('/',(request,response)=>{
-    return response.json({message:'Server Ok '})
-})
+app.get('/', (request, response) => {
+	response.json({ message: 'Server Ok ' });
+});
 
-app.listen('3333',()=>{
-    console.log('🎈Server Started in http://localhost:3333');
-    
-})
+app.listen('3333', () => {
+	console.log('🎈Server Started in http://localhost:3333');
+});
