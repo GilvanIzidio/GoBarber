6325ef721ba04fd16328d82bbb870b8d6e6b8094
Gilvan K <gilvam.izidio.dev@gmail.com>
2021-05-15 11:35:03 -0300

    desestruturado objeto

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -21,7 +21,7 @@ class AppointmentsRepository {
 	}
 
 	public create({ provider, date }: CreateAppointmentDTO): Appointments {
-		const appointment = new Appointments(provider, date);
+		const appointment = new Appointments({ provider, date });
 
 		this.bdAppointments.push(appointment);
 
