5b6c39df97f9d938110715e62afaf87573301f4f
Gilvan K <gilvam.izidio.dev@gmail.com>
2021-05-28 11:54:30 -0300

    Feat:<SignUp>Criado tela para cadastro de Usuarios

---
+++ frontend/src/pages/SignUp/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { Container, Content, Background } from './styles';
+import logoImg from '../../assets/logo.svg';
+import Button from '../../components/Button/index';
+import Input from '../../components/Input/index';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="nome" icon={FiUser} placeholder="Nome" />
+
+          <Input name="email" type="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="password"
+            type="password"
+            icon={FiLock}
+            placeholder="password"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+export default SignUp;
