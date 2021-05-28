694204f426fbb9b258c00e0db610ca5517865f76
Gilvan K <gilvam.izidio.dev@gmail.com>
2021-05-28 09:31:28 -0300

    refactor<dashboard> Criado componentes de Input e botao para reutilização

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
+import Button from '../../components/Button/index';
+import Input from '../../components/Input/index';
 
 const SignIn: React.FC = () => (
   <Container>
@@ -11,11 +13,11 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input type="email" placeholder="E-mail" />
+        <Input name="email" type="email" icon={FiMail} placeholder="E-mail" />
 
-        <input type="password" placeholder="Senha" />
+        <Input name="Botao" type="password" icon={FiLock} placeholder="Senha" />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
