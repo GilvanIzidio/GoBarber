9d2546f99bfe77ef5d734aa3fc97ffc2f36423bf
Gilvan K <gilvam.izidio.dev@gmail.com>
2021-06-02 14:54:57 -0300

    Feat<LogIn>Criado Tela do App
    feat:Foi implementado
    -rotas na aplicação
    -Componente de Input
    -Componente de Botão
    -Adicionado logo do app
    Trocado a fonte do App

---
+++ mobile/appgobarber/src/pages/SignIn/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { Container, Title } from './styles';
+import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu login</Title>
+
+      <Input name="email" icon="mail" placeholder="Email" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button>Entrar</Button>
+    </Container>
+  );
+};
+
+export default SignIn;
