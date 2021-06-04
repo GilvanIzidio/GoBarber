34ef7c5a6b3a78b2007f7d09aed9460a859c32cc
Gilvan K <gilvam.izidio.dev@gmail.com>
2021-06-04 17:58:33 -0300

    Feat:<autenticação>nessesario realizar refactor nesses arquivos

--- mobile/appgobarber/src/App.tsx
+++ mobile/appgobarber/src/App.tsx
@@ -2,8 +2,8 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import Routes from './routes';
-import AppProvider from './hooks';
+import Routes from './routes/index';
+import AppProvider from './hooks/index';
 
 const App: React.FC = () => (
   <NavigationContainer>
--- mobile/appgobarber/src/pages/SignIn/index.tsx
+++ mobile/appgobarber/src/pages/SignIn/index.tsx
@@ -60,6 +60,7 @@ const SignIn: React.FC = () => {
           email: data.email,
           password: data.password,
         });
+        Alert.alert('logado');
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
