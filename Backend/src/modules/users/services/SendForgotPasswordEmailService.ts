ed218ce8a4f367c0aa6ab079552645620fa9ff59
Gilvan K <gilvam.izidio.dev@gmail.com>
2021-06-11 09:08:40 -0300

    commit feito em tentativa de reversao de historico -arquivo com mais de 100mb em pasta

--- Backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ Backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -3,7 +3,6 @@ import AppError from '@shared/errors/AppError';
 
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
-
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
 interface IRequest {
@@ -30,9 +29,12 @@ class SendForgotPasswordEmailService {
 			throw new AppError('user does not exists');
 		}
 
-		await this.userTokensRepository.generate(user.id);
+		const { token } = await this.userTokensRepository.generate(user.id);
 
-		this.mailProvider.sendMail(email, 'recovery password');
+		await this.mailProvider.sendMail(
+			email,
+			`Pedido de recuperação de senha recebido ${token}`,
+		);
 	}
 }
 
