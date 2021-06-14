fe8e5caa18ac0ae58755452c1f512d0aabcd7852
Gilvan K <gilvam.izidio.dev@gmail.com>
2021-06-14 11:00:08 -0300

    Feat:<Profile>Adicionado service de Show -Criado controller para service de Show -Criado Rota para metodo Show e update do profile -Adicionado rota no index da aplicação -Ajustado nome da classe de UpdateProfileService

--- Backend/src/modules/users/services/UpdateProfileService.ts
+++ Backend/src/modules/users/services/UpdateProfileService.ts
@@ -12,7 +12,7 @@ interface IRequest {
 	password?: string;
 }
 @injectable()
-class UpdateProfile {
+class UpdateProfileService {
 	constructor(
 		@inject('UsersRepository')
 		private usersRepository: IUsersRepository,
@@ -64,4 +64,4 @@ class UpdateProfile {
 	}
 }
 
-export default UpdateProfile;
+export default UpdateProfileService;
