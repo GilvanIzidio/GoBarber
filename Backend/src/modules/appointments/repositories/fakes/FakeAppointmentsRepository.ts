ed662524d0c052b65545c89607c5c95680de27ca
Gilvan K <gilvam.izidio.dev@gmail.com>
2021-06-15 14:50:36 -0300

    Feat:adicionado Metodos Find nos arquivos metodos: findAllInMonthFromProvider findAllInDayFromProvider

--- Backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ Backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,7 +1,9 @@
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO';
 import { v4 as uuid } from 'uuid';
-import { isEqual } from 'date-fns';
+import { isEqual, getYear, getMonth, getDate } from 'date-fns';
 
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
@@ -14,6 +16,40 @@ class AppointmentsRepository implements IAppointmentsRepository {
 		);
 
 		return findAppointment;
+	}
+
+	public async findAllInMonthFromProvider({
+		provider_id,
+		month,
+		year,
+	}: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+		const appointments = this.appointments.filter(appointment => {
+			return (
+				appointment.provider_id === provider_id &&
+				getMonth(appointment.date) + 1 === month &&
+				getYear(appointment.date) === year
+			);
+		});
+
+		return appointments;
+	}
+
+	public async findAllInDayFromProvider({
+		provider_id,
+		month,
+		year,
+		day,
+	}: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+		const appointments = this.appointments.filter(appointment => {
+			return (
+				appointment.provider_id === provider_id &&
+				getMonth(appointment.date) + 1 === month &&
+				getYear(appointment.date) === year &&
+				getDate(appointment.date) === day
+			);
+		});
+
+		return appointments;
 	}
 
 	public async create({
